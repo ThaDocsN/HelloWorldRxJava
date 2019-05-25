c2a59d2acbefe4316397b5d870bcb699baf7195e
ThaDocIzN <rodan72@msn.com>
2019-05-24 20:29:29 -0500

    used buffer operator

--- app/src/main/java/com/thadocizn/helloworldrxjava/MainActivity.java
+++ app/src/main/java/com/thadocizn/helloworldrxjava/MainActivity.java
@@ -4,134 +4,51 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.ObservableSource;
+import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.functions.Function;
-import io.reactivex.observers.DisposableObserver;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String greeting = "Hello from RxJava. We're using Composite Disposable";
-    private Observable<Student>myObservable;
-    private DisposableObserver<Student> myObserver;
-    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+    private static final String TAG  = "Charles";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myObservable = Observable.create(new ObservableOnSubscribe<Student>() {
-            @Override
-            public void subscribe(ObservableEmitter<Student> emitter) throws Exception {
-                ArrayList<Student> studentArrayList = getStudents();
-
-                for (Student student : studentArrayList) {
-                    emitter.onNext(student);
-                }
-                emitter.onComplete();
-            }
-        });
-
-        compositeDisposable.add(myObservable.subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-                .concatMap(new Function<Student, ObservableSource<?>>() {
+        Observable<Integer>myObservable = Observable.range(1, 20);
+        myObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .buffer(4)
+                .subscribe(new Observer<List<Integer>>() {
                     @Override
-                    public ObservableSource<?> apply(Student student) throws Exception {
-                        Student student1=new Student();
-                        student1.setName(student.getName());
+                    public void onSubscribe(Disposable d) {
 
-                        Student student2=new Student();
-                        student2.setName("New member "+student.getName());
-
-                        student.setName(student.getName().toUpperCase());
-                        return Observable.just(student,student1,student2);
                     }
-                })
-        .subscribeWith(getObservable())
-        );
-    }
 
-    private DisposableObserver getObservable() {
-        myObserver = new DisposableObserver<Student>() {
-            @Override
-            public void onNext(Student s) {
+                    @Override
+                    public void onNext(List<Integer> integers) {
+                        Log.i(TAG," Came to onNext ");
 
+                        for (Integer i:integers) {
+                            Log.i(TAG, " int value is  "+i);
+                        }
+                    }
 
-                Log.i("Charles", "on next invoked" + s.getName());
+                    @Override
+                    public void onError(Throwable e) {
 
-            }
+                    }
 
-            @Override
-            public void onError(Throwable e) {
-                Log.i("Charles", "on error invoked");
-
-            }
-
-            @Override
-            public void onComplete() {
-                Log.i("Charles", "on complete invoked");
-
-            }
-        };
-        return myObserver;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        //compositeDisposable.dispose();//cant add more disposables
-
-        // if you want to add more disposables use clear method
-        compositeDisposable.clear();
-
-       // myObserver.dispose();
-    }
-
-    private ArrayList<Student> getStudents() {
-
-        ArrayList<Student> students = new ArrayList<>();
-
-        Student student1 = new Student();
-        student1.setName(" student 1");
-        student1.setEmail(" student1@gmail.com ");
-        student1.setAge(27);
-        students.add(student1);
-
-        Student student2 = new Student();
-        student2.setName(" student 2");
-        student2.setEmail(" student2@gmail.com ");
-        student2.setAge(20);
-        students.add(student2);
-
-        Student student3 = new Student();
-        student3.setName(" student 3");
-        student3.setEmail(" student3@gmail.com ");
-        student3.setAge(20);
-        students.add(student3);
-
-        Student student4 = new Student();
-        student4.setName(" student 4");
-        student4.setEmail(" student4@gmail.com ");
-        student4.setAge(20);
-        students.add(student4);
-
-        Student student5 = new Student();
-        student5.setName(" student 5");
-        student5.setEmail(" student5@gmail.com ");
-        student5.setAge(20);
-        students.add(student5);
-
-        return students;
-
-
+                    @Override
+                    public void onComplete() {
+                        Log.i(TAG," Came to onComplete ");
+                    }
+                });
     }
 }
